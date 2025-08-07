f08449d3acdfbe0be8dcc9638dd3d5e82b0b06f6
akash sandeepa <akashsandeepa11@gmail.com>
2025-08-07 11:44:44 +0530

    file structure created

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -8,12 +8,12 @@
         public function login(){
             $data=[];
 
-            $this->view('users/v_login', $data);
+            $this->view('pages/users/v_login', $data);
         }
 
         public function register(){
             $data = ['name'=>'akash'];
-            $this->view('users/v_register', $data);
+            $this->view('pages/users/v_register', $data);
         }
     }
 
