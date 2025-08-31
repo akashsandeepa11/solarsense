c55fb08291e5b5120a029458ea310b6d26026214
akash sandeepa <akashsandeepa11@gmail.com>
2025-08-31 19:24:45 +0530

    aah finaly conflicts resolved..🤧

--- app/views/inc/components/homeowner/summary_card.php
+++
@@ -1,9 +0,0 @@
-<?php
-// summary_card.php
-// Expects $card array with keys: label, value, icon
-?>
-<?php
-// DEPRECATED: moved to app/views/inc/components/homeowner_summary_card.php
-// No executable code here.
-?>
-<!-- homeowner/summary_card.php (deprecated) -->
